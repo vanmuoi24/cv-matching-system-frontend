851f4219cde907243e057b21c0d884708cd5c84b
Dai Do <doanhdaigr5.2004@gmail.com>
2026-02-12 22:30:20 +0700

    feat: Implement recruiter registration and admin user management CRUD operations.

--- src/features/auth/routes/AuthRoutes.tsx
+++ src/features/auth/routes/AuthRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import AuthLayout from '../layout/AuthLayout';
-import Register from '../pages/RegisterRE/page';
+import RegisterRE from '../pages/RegisterRE/page';
+import RegisterCA from '../pages/RegisterCA/page';
 import LoginCA from '../pages/LoginCA/page';
 import LoginRE from '../pages/LoginRE/page';
 
@@ -9,9 +10,9 @@ const AuthRoutes = () => {
 		<Routes>
 			<Route element={<AuthLayout />}>
 				<Route path='re/login' element={<LoginRE />} />
-				<Route path='re/register' element={<Register />} />
+				<Route path='re/register' element={<RegisterRE />} />
 				<Route path='ca/login' element={<LoginCA />} />
-				<Route path='ca/register' element={<Register />} />
+				<Route path='ca/register' element={<RegisterCA />} />
 			</Route>
 		</Routes>
 	);
